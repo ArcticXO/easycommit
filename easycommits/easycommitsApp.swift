ccad2ba09001e95802c68d14ab09ac383cf2a8bb
Ali Al Abdullah <38346718+arcticxo@users.noreply.github.com>
2024-02-02 14:52:45 +0000

    Initial Commit

---
+++ easycommits/easycommitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  easycommitsApp.swift
+//  easycommits
+//
+//  Created by Ali Al Abdullah on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct easycommitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
